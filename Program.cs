9a2b3007e21d959f00913d4ec9d81d817da3684d
WhitePaper233 <1423634265@qq.com>
2024-11-09 00:55:29 +0800

    Solution initialize

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Fleck;
+
+var server = new WebSocketServer("ws://0.0.0.0:8081");
+
+server.Start(socket =>
+{
+    socket.OnOpen = () => { Console.WriteLine("Client Connected"); };
+
+    socket.OnClose = () => { Console.WriteLine("Client Disconnected"); };
+
+    socket.OnMessage = message =>
+    {
+        Console.WriteLine("Received: " + message);
+        socket.Send(message);
+    };
+});
+
+Console.WriteLine("WebSocket Server is running on ws://0.0.0.0:8081");
+Console.WriteLine("Press Enter to stop the server...");
+Console.ReadLine();
